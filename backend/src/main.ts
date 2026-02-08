774c98712f4d106224eeac4f6222b25843e73aa8
ariel_11 <77409323+ariel11212321@users.noreply.github.com>
2026-02-08 13:58:42 +0200

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4200'],
+    origin: ['https://ig-assessment.onrender.com'],
     methods: ['GET', 'POST'],
     credentials: true,
   });
